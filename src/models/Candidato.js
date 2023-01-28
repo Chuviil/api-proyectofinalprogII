a6c1867a837be25e7aa0d3ea20f7788812eb705f
Sebeast <josueq12@gmail.com>
2023-01-28 14:28:05 -0500

    Arreglado el discriminador de Persona, no permitido para sus hijos ni padre.

--- src/models/Candidato.js
+++ src/models/Candidato.js
@@ -1,23 +1,17 @@
 import { Schema } from "mongoose";
 import Persona from "./Persona";
 
-const candidatoSchema = new Schema(
-  {
-    dignidad: {
-      type: String,
-      enum: ["ALCALDE", "PREFECTO", "CONCEJAL"],
-      required: true,
-    },
-    votos: {
-      type: Number,
-      default: 0,
-      required: true,
-    },
+const candidatoSchema = new Schema({
+  dignidad: {
+    type: String,
+    enum: ["ALCALDE", "PREFECTO", "CONCEJAL"],
+    required: true,
   },
-  {
-    timestamps: true,
-    versionKey: false,
-  }
-);
+  votos: {
+    type: Number,
+    default: 0,
+    required: true,
+  },
+});
 
 export default Persona.discriminator("Candidato", candidatoSchema);
